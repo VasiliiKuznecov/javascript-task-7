1cdcfa8cb99295375a1f3bea5dd471e22b48e2aa
Vasilii Kuznetsov <kvas@yandex-team.ru>
2017-11-23 19:21:25 +0500

    test

--- async.js
+++ async.js
@@ -7,7 +7,14 @@ exports.runParallel = runParallel;
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
+ *
+ * @returns {Promise} promise - промис
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
+function runParallel(jobs) {
+    return jobs[0]().then(data0 => {
+        return jobs[1]().then(data1 => {
+            return [data0, data1];
+        });
+    });
     // асинхронная магия
 }
